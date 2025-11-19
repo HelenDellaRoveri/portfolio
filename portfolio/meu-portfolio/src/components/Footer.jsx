b6e611e393368b7859173d70504fa0137ce590b7
Helen Yohanna Ventura Della Roveri <86626344+HelenDellaRoveri@users.noreply.github.com>
2025-11-19 09:35:01 -0300

    Delete portfolio/meu-portfolio directory

--- portfolio/meu-portfolio/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-900 text-white text-center py-6 mt-10">
-      <p>&copy; 2025 Helen Yohanna. Todos os direitos reservados.</p>
-    </footer>
-  );
-}
